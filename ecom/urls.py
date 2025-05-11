5f86e7a23b75ec6ae69ee846d1819d2f433dad39
AJEESH-MJ <ajeeshjayanm@gmail.com>
2025-05-11 05:39:34 +0530

    Added accounts and orders section

--- ecom/urls.py
+++ ecom/urls.py
@@ -23,7 +23,8 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('products.urls')),
-    path('user', include('users.urls'))
+    path('users/', include('users.urls')),
+    path('orders/', include('orders.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def show_acc(request):
-    return render(request, 'account.html')
+    return render(request, 'accounts.html')
